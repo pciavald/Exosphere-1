285555f80222ebd71227dcede744dd86b5ace510
Physes <philipsaunders@protonmail.com>
2016-07-20 09:57:12 -0300

    added block explorer

--- explorer/explorer1/app/routes/index.routes.parity.js
+++
@@ -1,9 +0,0 @@
-
-
-module.exports = function(app) {
-	var parity = require('../controllers/index.controllers.parity.js');
-	
-	app.use('/', function(req,res) {
-	res.render("index");	
-});
-}
--- explorer/explorer1/server.js
+++
@@ -1,7 +0,0 @@
-var express = require('./config/express.js');
-var app = express();
-
-app.listen(3000);
-module.exports = app;
-
-console.log('Server running at http://localhost:3000');
